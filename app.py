fbad911231498c60191e3b56f13536d9fa46d997
Cindzzi <adeliatina.manuel@gmail.com>
2019-11-27 14:02:09 -0600

    created csv folder/ python api/

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import os
-
 import pandas as pd
 import numpy as np
-
+import requests
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+import pymongo
+
+
 
 app = Flask(__name__)
 
@@ -20,31 +22,42 @@ app = Flask(__name__)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = " URL here"
 # db = SQLAlchemy(app)
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/project2rice_app")
+conn = 'mongodb://localhost:27017/project2rice_app'
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+db = client.heroku_8r6fdxgl
+collection = db.Droughts
+collection = db.floods
+collection = db.fires
+
+
 
 # Base = automap_base()
 # Base.prepare(db.engine, reflect=True)
 
 #create route that renders index.html
 @app.route("/")
-def home():
+def Home():
     """Return the homepage."""
     return render_template("index.html")
 
 
-# @app.route("/api/categories")
-# def categories():
+@app.route("/geomap")
+def geomap():
     
-#     return jsonify()
+    return render_template("comparison.html")
 
-# @app.route("/api/#extremeweather")
-# def extremeweather():
+@app.route("/categories")
+def categories():
 
-#     return jsonify(data)
+    return render_template("photos2.html")
 
-# @app.route("/api/#severeweather")
-# def jhg():
+# @app.route("/data")
+# def data():
 
-    return jsonify(data)
+#     return jsonify("data.html")
 
 if __name__ == "__main__":
     app.run()
