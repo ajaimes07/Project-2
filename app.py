225214b69c2344e898ffd7292a587eb28f93d3ec
Cindzzi <adeliatina.manuel@gmail.com>
2019-12-01 13:04:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-import os
-import pandas as pd
-import numpy as np
 import requests
 from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
 import pymongo
+import sys
+
 
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
-
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/project2rice_app")
 conn = 'mongodb://localhost:27017/project2rice_app'
 client = pymongo.MongoClient(conn)
 
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/project2rice_app'
+
 # connect to mongo db and collection
 db = client.heroku_8r6fdxgl
-collection = db.Droughts
 collection = db.floods
 collection = db.fires
+
 
 #create route that renders index.html
 @app.route("/")
@@ -28,23 +24,19 @@ def Home():
     """Return the homepage."""
     return render_template("index.html")
 
-
 @app.route("/wildfire")
 def wildfire():
-    
-    return render_template("wildfire.html")
-
-@app.route("/hurricane")
-def hurricane():
-
-    return render_template("hurricane.html")
+    # write a statement that finds all the items in the db and sets it to a variable
+    fires = list(db.collection.find())
+    print(fires)
+    return render_template("wildfire.html", fires=fires)
 
 @app.route("/flood")
 def flood():
-
-    return render_template("flood.html")
- # Redirect back to home page
-    return redirect("/")
+    # write a statement that finds all the items in the db and sets it to a variable 
+    floods = list(db.collection.find())
+    print(floods)
+    return render_template("flood.html",floods=floods)
 
 if __name__ == "__main__":
     app.run()
