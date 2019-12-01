3703751ec722e4e131c225d0af97d73f7a399caa
Cindzzi <adeliatina.manuel@gmail.com>
2019-12-01 15:27:14 -0600

    most recent version

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ import sys
 
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017/project2rice_app'
-client = pymongo.MongoClient(conn)
+# conn = 'mongodb://localhost:27017/project2rice_app'
+# client = pymongo.MongoClient(conn)
 
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/project2rice_app'
+app.config['MONGO_DBNAME'] = 'project2rice'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/project2rice'
 
+mongo = PyMongo(app)
 # connect to mongo db and collection
-db = client.heroku_8r6fdxgl
-collection = db.floods
-collection = db.fires
-
+# db = client.heroku_8r6fdxgl
+# collection = db.floods
+# collection = db.fires
 
 #create route that renders index.html
 @app.route("/")
@@ -24,12 +25,13 @@ def Home():
     """Return the homepage."""
     return render_template("index.html")
 
-@app.route("/wildfire")
-def wildfire():
-    # write a statement that finds all the items in the db and sets it to a variable
-    fires = list(db.collection.find())
-    print(fires)
-    return render_template("wildfire.html", fires=fires)
+@app.route("/fire", methods=['GET'])
+def get_all_fires():
+    fires = mongo.db.fires
+    output = []
+    for f in fires.find() :
+        output.append({'location': f['location'], 'date' : f['$date']})
+    return jsonify({'result' : output})
 
 @app.route("/flood")
 def flood():
@@ -39,4 +41,4 @@ def flood():
     return render_template("flood.html",floods=floods)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
